a05894c2c3fee5bd27aa69d25bf715b7575f1dfe
Palvi Dhawan <palvidhawan@Palvis-MacBook-Pro.local>
2021-09-20 02:08:20 +0530

    b-webpack | Extract CSS into separate bundle

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
 
+const heading = new Heading();
 const helloButton = new HelloWorldButton();
+heading.render();
 helloButton.render();
